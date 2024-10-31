c3a76a85f5743ead9aeb3f6905c0978658de219a
DominicGBauer <dominic@nomanini.com>
2024-10-31 16:48:42 +0200

    chore: remove secrets

--- Demo/PowerSyncExample/_Secrets.swift
+++ Demo/PowerSyncExample/_Secrets.swift
@@ -2,7 +2,7 @@ import Foundation
 
 // Enter your Supabase and PowerSync project details.
 enum Secrets {
-    static let powerSyncEndpoint = "https://654a826e1b70717c8dc85790.powersync.journeyapps.com"
-    static let supabaseURL = URL(string: "https://whlrfrfknkhckmffonio.supabase.co")!
-    static let supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndobHJmcmZrbmtoY2ttZmZvbmlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkzODE0ODYsImV4cCI6MjAxNDk1NzQ4Nn0.gUTu3LDFetyda4Hd2gZLtv9o8pvCxEjDjm6DCVjrehw"
+    static let powerSyncEndpoint = "https://your-id.powersync.journeyapps.com"
+    static let supabaseURL = URL(string: "https://your-id.supabase.co")!
+    static let supabaseAnonKey = "anon-key"
 }
