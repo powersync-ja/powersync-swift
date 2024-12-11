a3698c491be65da7473ce7abfaed6307bcecf4de
benitav <benita@journeyapps.com>
2024-12-11 17:18:33 +0200

    Closing bracket

--- Demo/PowerSyncExample/_Secrets.swift
+++ Demo/PowerSyncExample/_Secrets.swift
@@ -5,3 +5,4 @@ enum Secrets {
     static let powerSyncEndpoint = "https://your-id.powersync.journeyapps.com"
     static let supabaseURL = URL(string: "https://your-id.supabase.co")!
     static let supabaseAnonKey = "anon-key"
+}
