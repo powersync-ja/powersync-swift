aa6091799c67976a7824960ea1ef44a1684c985e
stevensJourney <steven@journeyapps.com>
2025-04-14 10:16:34 +0200

    remove comma

--- Demo/PowerSyncExample/_Secrets.swift
+++ Demo/PowerSyncExample/_Secrets.swift
@@ -4,7 +4,7 @@ import Foundation
 enum Secrets {
     static let powerSyncEndpoint = "https://your-id.powersync.journeyapps.com"
     static let supabaseURL = URL(string: "https://your-id.supabase.co")!
-    static let supabaseAnonKey = "anon-key",
+    static let supabaseAnonKey = "anon-key"
     // Optional storage bucket name. Set to nil if you don't want to use storage.
     static let supabaseStorageBucket = "media"
 }
