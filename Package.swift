798c533fad19dd92d91a6e51c85630443693172a
stevensJourney <steven@journeyapps.com>
2025-05-02 15:11:00 +0200

    update package

--- Package.swift
+++ Package.swift
@@ -17,8 +17,7 @@ let package = Package(
             targets: ["PowerSync"]),
     ],
     dependencies: [
-        .package(path: "/Users/stevenontong/Documents/platform_code/powersync/powersync-kotlin"),
-        // .package(url: "https://github.com/powersync-ja/powersync-kotlin.git", exact: "1.0.0-BETA32.0"),
+        .package(url: "https://github.com/powersync-ja/powersync-kotlin.git", "1.0.1+SWIFT.0"..<"1.1.0+SWIFT.0"),
         .package(url: "https://github.com/powersync-ja/powersync-sqlite-core-swift.git", "0.3.14"..<"0.4.0")
     ],
     targets: [
