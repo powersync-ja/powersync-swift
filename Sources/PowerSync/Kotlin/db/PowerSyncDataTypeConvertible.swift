4f43c25872fbcbda4b33c5aa1f0ef1b7ffdab8e0
Jairon Terrero <jairon@passivelogic.com>
2025-10-24 11:35:35 -0600

    feat: Add PowerSyncDataConvertible protocol
    This enables downstream consumers to define how certain types should be mapped to those that are supported by the PowerSync Kotlin Multiplatform SDK.

---
+++ Sources/PowerSync/Kotlin/db/PowerSyncDataTypeConvertible.swift
@@ -0,0 +1,32 @@
+import struct Foundation.Data
+
+/// Represents the set of types that are supported
+/// by the PowerSync Kotlin Multiplatform SDK
+public enum PowerSyncDataType {
+    case bool(Bool)
+    case string(String)
+    case int64(Int64)
+    case int32(Int32)
+    case double(Double)
+    case data(Data)
+}
+
+/// Types conforming to this protocol will be
+/// mapped to the specified ``PowerSyncDataType``
+/// before use by the PowerSync Kotlin Multiplatform SDK
+public protocol PowerSyncDataTypeConvertible {
+    var psDataType: PowerSyncDataType? { get }
+}
+
+extension PowerSyncDataType {
+    func unwrap() -> Any {
+        switch self {
+            case let .bool(bool): bool
+            case let .string(string): string
+            case let .int32(int32): int32
+            case let .int64(int64): int64
+            case let .double(double): double
+            case let .data(data): data
+        }
+    }
+}
--- Tests/PowerSyncTests/Kotlin/KotlinPowerSyncDatabaseImplTests.swift
+++ Tests/PowerSyncTests/Kotlin/KotlinPowerSyncDatabaseImplTests.swift
@@ -1,4 +1,5 @@
 @testable import PowerSync
+import struct Foundation.UUID
 import XCTest
 
 final class KotlinPowerSyncDatabaseImplTests: XCTestCase {
@@ -153,6 +154,25 @@ final class KotlinPowerSyncDatabaseImplTests: XCTestCase {
             XCTFail("Expected an error to be thrown")
         } catch {
             XCTAssertEqual(error.localizedDescription, "cursor error")
+        }
+    }
+
+    func testCustomDataTypeConvertible() async throws {
+        let uuid = UUID()
+        try await database.execute(
+            sql: "INSERT INTO users (id, name, email) VALUES (?, ?, ?)",
+            parameters: [uuid, "Test User", "test@example.com"]
+        )
+
+        let _ = try await database.getOptional(
+            sql: "SELECT id, name, email FROM users WHERE id = ?",
+            parameters: [uuid]
+        ) { cursor throws in
+           try (
+                cursor.getString(name: "id"),
+                cursor.getString(name: "name"),
+                cursor.getString(name: "email")
+            )
         }
     }
 
@@ -623,5 +643,12 @@ final class KotlinPowerSyncDatabaseImplTests: XCTestCase {
         XCTAssertEqual(result.count, 2)
         XCTAssertEqual(result[0], JoinOutput(name: "Test User", description: "task 1", comment: "comment 1"))
         XCTAssertEqual(result[1], JoinOutput(name: "Test User", description: "task 2", comment: "comment 2"))
+    }
+}
+
+
+extension UUID: @retroactive PowerSyncDataTypeConvertible {
+    public var psDataType: PowerSyncDataType? {
+        .string(uuidString)
     }
 }
